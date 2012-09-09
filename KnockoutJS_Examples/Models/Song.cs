d621fa55d1cbf018ca468eb51dfdcf54da63fc97
Steve Duitsman <steve.duitsman+github@gmail.com>
2012-09-09 16:29:58 -0500

    VS2010 version of VS2012 examples

---
+++ KnockoutJS_Examples/Models/Song.cs
@@ -0,0 +1,11 @@
+﻿
+namespace KnockoutJS_Examples.Models
+{
+	public class Song
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Artist { get; set; }
+		//public TimeSpan Duration { get; set; }
+	}
+}
